061f52f4f0c0644660f30baa7dcf9348a1adafb5
Matt Robitaille <matt@anchour.com>
2021-02-12 16:46:37 -0500

    Comments on button.js

--- src/plugins/button.js
+++ src/plugins/button.js
@@ -1,5 +1,16 @@
 const plugin = require('tailwindcss/plugin');
 
+/**
+ * This is a sensible base .btn style. Active states *probably* should be created
+ * on a per-project basis, determined by the style guide for that particular
+ * project.
+ *
+ * We may be able to make this a bit more configurable if we *only* use
+ * the `theme` function below to pull in configuration values from the
+ * tailwind.config.js file. Alternatively, we could explore using a
+ * different config object within the Tailwind config dedicated
+ * specifically to buttons.
+ */
 module.exports = plugin(function ({ addComponents, theme }) {
   const components = {
     '.btn': {
